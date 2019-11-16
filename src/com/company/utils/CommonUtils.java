3f5e2a2dd0f2561956d60f762176551b9f499ca3
Hamed Ghaderian <HamedGhaderian@users.noreply.github.com>
2019-11-16 22:33:13 +0330

    add email validation method to CommonUtils

--- src/com/company/utils/CommonUtils.java
+++ src/com/company/utils/CommonUtils.java
@@ -1,6 +1,11 @@
 package com.company.utils;
 
 public class CommonUtils {
+
+    //
+    public static final String EMAIL_PATTERN = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+
     public static boolean isCellNumberValid(String number) {
         if (number != null && !number.isEmpty()) {
             if (number.length() == 11 && number.startsWith("09")) {
@@ -9,4 +14,15 @@ public class CommonUtils {
         }
         return false;
     }
+
+    public static boolean isEmailValid(String email) {
+        if (email != null && !email.isEmpty()) {
+            if (email.matches(EMAIL_PATTERN)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
